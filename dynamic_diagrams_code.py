93ea1e93d55a7790433aef0d51aa34ad1fc79b62
winadm1n <winadm1n@protonmail.com>
2024-05-29 17:04:59 +0000

    Move files to new folder

--- dynamic_diagrams_code.py
+++ dynamic_diagrams_code.py
@@ -31,20 +31,20 @@ def get_image_urls(query):
 
 with Diagram("architecture_diagram", show=False):
 
-    urlretrieve(get_image_urls("icon of Sentiment Score in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Sentiment_Score_icon.png")
-    Sentiment_Score_ = Custom("Sentiment Score \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Sentiment_Score_icon.png")
-
-    urlretrieve(get_image_urls("icon of Positive/Negative Result in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Positive_Negative_Result_icon.png")
-    Positive_Negative_Result_ = Custom("Positive/Negative Result \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Positive_Negative_Result_icon.png")
+    urlretrieve(get_image_urls("icon of Text Data in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Text_Data_icon.png")
+    Text_Data_ = Custom("Text Data \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Text_Data_icon.png")
 
     urlretrieve(get_image_urls("icon of Azure Cognitive Services in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Azure_Cognitive_Services_icon.png")
     Azure_Cognitive_Services_ = Custom("Azure Cognitive \nServices ", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Azure_Cognitive_Services_icon.png")
 
+    urlretrieve(get_image_urls("icon of Positive/Negative Result in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Positive_Negative_Result_icon.png")
+    Positive_Negative_Result_ = Custom("Positive/Negative Result \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Positive_Negative_Result_icon.png")
+
+    urlretrieve(get_image_urls("icon of Sentiment Score in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Sentiment_Score_icon.png")
+    Sentiment_Score_ = Custom("Sentiment Score \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Sentiment_Score_icon.png")
+
     urlretrieve(get_image_urls("icon of Sentiment Analysis in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Sentiment_Analysis_icon.png")
     Sentiment_Analysis_ = Custom("Sentiment Analysis \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Sentiment_Analysis_icon.png")
-
-    urlretrieve(get_image_urls("icon of Text Data in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Text_Data_icon.png")
-    Text_Data_ = Custom("Text Data \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/temp_src_images/Text_Data_icon.png")
 
 
     Text_Data_ >> Azure_Cognitive_Services_
