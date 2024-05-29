2adbc9cc9ccfc0343fe020167be16b07e197a06d
winadm1n <winadm1n@protonmail.com>
2024-05-29 18:05:51 +0000

    Move files to new folder

--- dynamically_generated_stuff/dynamic_diagrams_code.py
+++ dynamically_generated_stuff/dynamic_diagrams_code.py
@@ -29,22 +29,22 @@ def get_image_urls(query):
             image_urls.append(img_url)
     return image_urls[1]
 
-with Diagram("/home/runner/work/Archimage_Generator/Archimage_Generator/Architecture_images/architecture_diagram", show=False):
+with Diagram("Architecture_images/architecture_diagram.png", show=False):
+
+    urlretrieve(get_image_urls("icon of iron man in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/iron_man_icon.png")
+    iron_man_ = Custom("iron man \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/iron_man_icon.png")
+
+    urlretrieve(get_image_urls("icon of Sentiment Analysis in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Sentiment_Analysis_icon.png")
+    Sentiment_Analysis_ = Custom("Sentiment Analysis \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Sentiment_Analysis_icon.png")
+
+    urlretrieve(get_image_urls("icon of Positive/Negative Result in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Positive_Negative_Result_icon.png")
+    Positive_Negative_Result_ = Custom("Positive/Negative Result \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Positive_Negative_Result_icon.png")
 
     urlretrieve(get_image_urls("icon of Azure Cognitive Services in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Azure_Cognitive_Services_icon.png")
     Azure_Cognitive_Services_ = Custom("Azure Cognitive \nServices ", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Azure_Cognitive_Services_icon.png")
 
     urlretrieve(get_image_urls("icon of azure cloud functions in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/azure_cloud_functions_icon.png")
     azure_cloud_functions_ = Custom("azure cloud \nfunctions ", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/azure_cloud_functions_icon.png")
-
-    urlretrieve(get_image_urls("icon of iron man in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/iron_man_icon.png")
-    iron_man_ = Custom("iron man \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/iron_man_icon.png")
-
-    urlretrieve(get_image_urls("icon of Positive/Negative Result in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Positive_Negative_Result_icon.png")
-    Positive_Negative_Result_ = Custom("Positive/Negative Result \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Positive_Negative_Result_icon.png")
-
-    urlretrieve(get_image_urls("icon of Sentiment Analysis in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Sentiment_Analysis_icon.png")
-    Sentiment_Analysis_ = Custom("Sentiment Analysis \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Sentiment_Analysis_icon.png")
 
 
     azure_cloud_functions_ >> Azure_Cognitive_Services_
