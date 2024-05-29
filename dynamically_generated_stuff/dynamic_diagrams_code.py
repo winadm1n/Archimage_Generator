1d8971a4e4b3a6fb6b28d8b316e23a021b1b0ef2
winadm1n <winadm1n@protonmail.com>
2024-05-29 18:43:22 +0000

    Move files to new folder

--- dynamically_generated_stuff/dynamic_diagrams_code.py
+++ dynamically_generated_stuff/dynamic_diagrams_code.py
@@ -28,24 +28,24 @@ def get_image_urls(query):
         if img_url:
             image_urls.append(img_url)
     return image_urls[1]
-with Diagram("Architecture_images/hanuman", show=False):
+with Diagram("Architecture_images/lokesh", show=False):
     urlretrieve(get_image_urls("icon of azure cloud functions in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/azure_cloud_functions_icon.png")
     azure_cloud_functions_ = Custom("azure cloud \nfunctions ", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/azure_cloud_functions_icon.png")
+
+    urlretrieve(get_image_urls("icon of bal hanuman cute in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/bal_hanuman_cute_icon.png")
+    bal_hanuman_cute_ = Custom("bal hanuman \ncute ", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/bal_hanuman_cute_icon.png")
 
     urlretrieve(get_image_urls("icon of cosmos effect in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/cosmos_effect_icon.png")
     cosmos_effect_ = Custom("cosmos effect \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/cosmos_effect_icon.png")
 
-    urlretrieve(get_image_urls("icon of Azure Cognitive Services in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Azure_Cognitive_Services_icon.png")
-    Azure_Cognitive_Services_ = Custom("Azure Cognitive \nServices ", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Azure_Cognitive_Services_icon.png")
-
-    urlretrieve(get_image_urls("icon of hanuman in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/hanuman_icon.png")
-    hanuman_ = Custom("hanuman ", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/hanuman_icon.png")
-
     urlretrieve(get_image_urls("icon of Sentiment Analysis in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Sentiment_Analysis_icon.png")
     Sentiment_Analysis_ = Custom("Sentiment Analysis \n", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Sentiment_Analysis_icon.png")
+
+    urlretrieve(get_image_urls("icon of Azure Cognitive Services in AZURE"), "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Azure_Cognitive_Services_icon.png")
+    Azure_Cognitive_Services_ = Custom("Azure Cognitive \nServices ", "/home/runner/work/Archimage_Generator/Archimage_Generator/dynamically_generated_stuff/temp_src_images/Azure_Cognitive_Services_icon.png")
 
 
     azure_cloud_functions_ >> Azure_Cognitive_Services_
     Azure_Cognitive_Services_ >> Sentiment_Analysis_
-    Sentiment_Analysis_ >> hanuman_
-    hanuman_ >> cosmos_effect_
+    Sentiment_Analysis_ >> bal_hanuman_cute_
+    bal_hanuman_cute_ >> cosmos_effect_
