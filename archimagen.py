cafca597461c2c52637d77ba30828404c8a1248d
winadm1n <85485845+winadm1n@users.noreply.github.com>
2024-05-29 23:23:36 +0530

    Update archimagen.py

--- archimagen.py
+++ archimagen.py
@@ -81,7 +81,7 @@ def get_image_urls(query):
             image_urls.append(img_url)
     return image_urls[1]
 
-with Diagram("architecture_diagram", show=False):
+with Diagram("/home/runner/work/Archimage_Generator/Archimage_Generator/Architecture_images", show=False):
 """
 services = re.findall(r"(['\"])(.*?)\1", edges_text)
 services = list(set([match[1] for match in services]))
